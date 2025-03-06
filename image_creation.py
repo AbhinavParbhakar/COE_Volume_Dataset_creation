0bca5f53b9c90f391846a1a7978255f0804498c8
Abhinav parbhakar <abhinav.parbhakar302@gmail.com>
2025-03-06 16:42:32 -0700

    Downloaded all of the images

--- image_creation.py
+++ image_creation.py
@@ -7,6 +7,8 @@ import ast
 from playwright.sync_api import sync_playwright
 import os
 import time
+from multiprocessing import Pool
+import multiprocessing
 
 
 def shapely_to_ee_geom(line:LineString)->ee.geometry.Geometry.LineString:
@@ -59,7 +61,7 @@ def create_zoomed_in_images(file_name,save_name='Image.png',delete_html_file=Fal
         context = browser.new_context()
         page = context.new_page()
         page.goto(file_name)
-        time.sleep(5)
+        time.sleep(30)
         page.screenshot(path=save_name,full_page=True,clip=screenshot_settings)
     
     if delete_html_file:
@@ -71,28 +73,47 @@ def create_granular_output(line:ee.geometry.Geometry,file_name:str):
     Given the line and the file name, created images with the Estimation Id as the name and 
     stores it in images folder
     """
+    ee.Authenticate()
+    ee.Initialize(project='ee-city-of-edmonton-modeling')
     html_file = create_output_html(line,file_name)
     estimation_point_id = os.path.basename(html_file).split('.')[0]
     save_name = f'./data/images/{estimation_point_id}.png'
     create_zoomed_in_images(html_file,save_name=save_name,delete_html_file=True)
 
-def create_granular_images(data_file)->list[str]:
+def create_granular_images(data_file,target_list=None)->list[str]:
     """
     Given the data file containing geometries, see what one looks like
-    on the screen.
+    on the screen. Capable of only using the rows where the ID matches value in target
     """
         
     ee.Authenticate()
     ee.Initialize(project='ee-city-of-edmonton-modeling')
     df = pd.read_csv(data_file)
-    df['ee_geometry'] = df.apply(lambda x: shapely_to_ee_geom(loads(x['road_geo'])),axis=1)
-    file_names = df.apply(lambda x: create_granular_output(x['ee_geometry'],f'./data/html_files/{x['Estimation_point']}.html'),axis=1)
     
-    return file_names
+    if target_list:
+        df = df[df.apply(lambda x: x['Estimation_point'] in target_list,axis=1)]
+    
+    df['ee_geometry'] = df.apply(lambda x: shapely_to_ee_geom(loads(x['road_geo'])),axis=1)
+    arguments = df.apply(lambda x: (x['ee_geometry'],f'./data/html_files/{x['Estimation_point']}.html'),axis=1)
+    
+    with Pool(multiprocessing.cpu_count()) as p:
+        p.starmap(create_granular_output,iterable=arguments.tolist())    
+    return None
+
+
     
 
 if __name__ == "__main__":
     data_file = './data/excel_files/data_with_lines.csv'
-    create_granular_images(data_file)
-        
+    
+    files_to_delete = [102996, 104526, 514704, 514712, 515176, 530565, 530695, 530699, 530735, 530739, 530743, 966715, 967572, 1007611, 1017467, 1017468, 1095481, 1123698, 1121514, 1123614] 
+    create_granular_images(data_file,files_to_delete)
+    
+    # file_path = f'C:\\Users\\abhin\\OneDrive\\Documents\\Computing\\Research\\City of Edmonton Volume Prediction\\City of Edmonton Volume Data Creation\\data\\images'
+   
+    # files = [f'{file_path}\\{image}.png'  for image in files_to_delete]
+    
+    # for file in files:
+    #     os.remove(file)
+    # print('Done')    
         
